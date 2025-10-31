d6944e3e2f0b36e7c9dd73322fccb2b3c6f58b90
Przemek <przemekgg2002@gmail.com>
2025-10-31 14:02:58 +0100

    add: audio wave to audio item view

--- s/editor/ui/pages/project/tabbing/tabs/edit/views/items/audio/view.ts
+++ s/editor/ui/pages/project/tabbing/tabs/edit/views/items/audio/view.ts
@@ -1,7 +1,7 @@
 
 import {html} from "lit"
 import {view} from "@e280/sly"
-import {Item} from "@omnimedia/omnitool"
+import {Item, Waveform} from "@omnimedia/omnitool"
 
 import {TimelineItem} from "../parts/item/view.js"
 import themeCss from "../../../../../../../../../theme.css.js"
@@ -10,7 +10,20 @@ import {EditorContext} from "../../../../../../../../../context/context.js"
 export const TimelineAudio = view(use => (context: EditorContext, clip: Item.Audio, ancestors: Item.Any[]) => {
 	use.styles(themeCss)
 
-	return html`${TimelineItem(context, clip, html``, ancestors)}`}
+	const wave = use.once(() => {
+		const wave = document.createElement("div")
+		wave.style.pointerEvents = "none"
+		const waveform = Waveform.init(context.driver, "/assets/temp/gl.mp4", wave)
+		waveform.then(w => w.wavesurfer.setOptions({
+			height: 35,
+			barAlign: "bottom",
+			barWidth: undefined,
+			waveColor: "rgb(3, 148, 129)"
+		}))
+		return wave
+	})
+
+	return html`${TimelineItem(context, clip, html`${wave}`, ancestors)}`}
 )
 
 
